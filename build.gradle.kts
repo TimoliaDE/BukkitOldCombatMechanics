e3f7795a4774e6708360ac13ac0ac455701d5a69
icefear <minhnhatvu04@gmail.com>
2026-05-22 01:24:58 +0200

    fix: downgrade java version for more compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,9 +65,9 @@ dependencies {
 }
 
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 25 on systems
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems
     // that only have JDK 11 installed for example.
-    toolchain.languageVersion = JavaLanguageVersion.of(25)
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
 tasks {
@@ -83,7 +83,7 @@ tasks {
 
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
-        options.release.set(25)
+        options.release.set(21)
     }
     javadoc {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
